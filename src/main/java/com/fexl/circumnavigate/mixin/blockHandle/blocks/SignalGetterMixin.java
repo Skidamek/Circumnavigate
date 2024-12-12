d35ac739f80977c4062650db069b330bd371adb3
Nathan Benson <87458603+FamroFexl@users.noreply.github.com>
2024-12-11 18:00:29 -0700

    Fixed production-only issues for version 0.5.

--- src/main/java/com/fexl/circumnavigate/mixin/blockHandle/blocks/SignalGetterMixin.java
+++ src/main/java/com/fexl/circumnavigate/mixin/blockHandle/blocks/SignalGetterMixin.java
@@ -6,6 +6,7 @@ package com.fexl.circumnavigate.mixin.blockHandle.blocks;
 
 import com.fexl.circumnavigate.processing.BlockPosWrapped;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.SignalGetter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,22 +18,28 @@ public interface SignalGetterMixin {
 	@ModifyVariable(method = "getControlInputSignal", at = @At("HEAD"), index = 1, argsOnly = true)
 	default BlockPos modifyBlockPos(BlockPos blockPos) {
 		SignalGetter thiz = (SignalGetter) (Object) this;
-		if(thiz.isClientSide()) return blockPos;
-		return new BlockPosWrapped(blockPos, thiz.getTransformer());
+		if(thiz instanceof ServerLevel serverLevel) {
+			return new BlockPosWrapped(blockPos, serverLevel.getTransformer());
+		}
+		return blockPos;
 	}
 
 	@ModifyVariable(method = "getSignal", at = @At("HEAD"), index = 1, argsOnly = true)
 	default BlockPos modifyBlockPos2(BlockPos blockPos) {
 		SignalGetter thiz = (SignalGetter) (Object) this;
-		if(thiz.isClientSide()) return blockPos;
-		return new BlockPosWrapped(blockPos, thiz.getTransformer());
+		if(thiz instanceof ServerLevel serverLevel) {
+			return new BlockPosWrapped(blockPos, serverLevel.getTransformer());
+		}
+		return blockPos;
 	}
 
 	@ModifyVariable(method = "hasNeighborSignal", at = @At("HEAD"), index = 1, argsOnly = true)
 	default BlockPos modifyBlockPos3(BlockPos blockPos) {
 		SignalGetter thiz = (SignalGetter) (Object) this;
-		if(thiz.isClientSide()) return blockPos;
-		return new BlockPosWrapped(blockPos, thiz.getTransformer());
+		if(thiz instanceof ServerLevel serverLevel) {
+			return new BlockPosWrapped(blockPos, serverLevel.getTransformer());
+		}
+		return blockPos;
 	}
 
 
